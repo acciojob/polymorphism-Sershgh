984295525005a58bec58233b132188cc6ad4cb22
unknown <sounakray18@gmail.com>
2023-09-21 12:54:08 +0530

    polymorphism ass is added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
-public class Main {
-
+public class Main{
+public static void main(String args[]){
+   product p=new product();
+   int ans=p.product(10,20);
+   int ans1=p.product(10,20,30);
+   double ans2=p.product(1.0,2.0);
+   System.out.println(ans+" "+ans1+" "+ans2);
+}
+static class product{
+public int product(int x,int y){
+    return x*y;
+}
+public int product(int x,int y,int z){
+    return x*y*z;
+}
+public double product(double x,double y){
+    return x*y;
+}
+}
 }
